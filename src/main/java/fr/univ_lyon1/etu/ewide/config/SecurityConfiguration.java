bf3b46d8378b5cb4896ad3fedc6218127ab932ba
Arthur POIRET <dropsnorz@gmail.com>
2016-10-17 09:12:37 +0200

    Setup spring security

---
+++ src/main/java/fr/univ_lyon1/etu/ewide/config/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package fr.univ_lyon1.etu.ewide.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+ 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+ 
+ 
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+
+    	auth.inMemoryAuthentication().withUser("admin@ewide.com").password("root123").roles("ADMIN");
+
+    }
+    
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+        .antMatchers("/dashboard").access("hasRole('ADMIN')")
+        .and().formLogin().loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/dashboard", true)
+        .usernameParameter("email").passwordParameter("password")
+        .and().csrf()
+        .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+ 
+}
