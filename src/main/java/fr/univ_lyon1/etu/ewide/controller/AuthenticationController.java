942fced5e80b3f3679c3f2d9ad85cbcaa716d677
Arthur POIRET <dropsnorz@gmail.com>
2016-10-20 15:53:55 +0200

    Redirect on /login if user already authenticated, Bug #4075

--- src/main/java/fr/univ_lyon1/etu/ewide/controller/AuthenticationController.java
+++ src/main/java/fr/univ_lyon1/etu/ewide/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -34,6 +35,12 @@ public class AuthenticationController {
 
 	@RequestMapping(value ="/login",  method = RequestMethod.GET)
 	public String connexion() {
+		
+		if(authenticationUserService.isCurrentUserLogged()){
+			
+			return "redirect:/dashboard";
+		}
+		
 		return "login";
 	}
 
