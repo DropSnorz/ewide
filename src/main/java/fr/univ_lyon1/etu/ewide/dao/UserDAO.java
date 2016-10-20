212c54be45d3a8e9f3ddbc37b67c71fa1e2402c9
Arthur POIRET <dropsnorz@gmail.com>
2016-10-20 10:43:34 +0200

    Added user registration logic, Feature #4057

---
+++ src/main/java/fr/univ_lyon1/etu/ewide/dao/UserDAO.java
@@ -0,0 +1,77 @@
+package fr.univ_lyon1.etu.ewide.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import fr.univ_lyon1.etu.ewide.Model.Project;
+import fr.univ_lyon1.etu.ewide.Model.User;
+
+/**
+ * Created by Firas on 17/10/2016.
+ */
+
+@Repository
+public class UserDAO {
+	
+	@Autowired
+	protected EntityManagerFactory entityManagerFactory;
+	
+	
+	@PersistenceContext
+	protected EntityManager em;
+	
+	/**
+     * Renvoie l'utilisateur correspondant à cet email
+     * @param email l'email de l'utilisateur
+     * @return l'utilisateur ou null s'il n'existe pas
+     */
+	public User getUserByEmail(String email) {
+		
+		
+        try {
+			TypedQuery<User> query = em.createQuery("SELECT user FROM User user "
+					+ "WHERE user.mail = :email_value", User.class)
+					.setParameter("email_value", email);
+			return query.getSingleResult();
+		} catch (Exception e) {
+
+			return null;
+		}
+    
+    }
+	public User getUserByUsername(String username){
+		 try {
+				TypedQuery<User> query = em.createNamedQuery("User.getByUsername", User.class);
+				query.setParameter("username", username);
+				return query.getSingleResult();
+			} catch (Exception e) {
+
+				return null;
+			}
+	}
+			
+	/**
+     * Créée un nouvel utilisateur ou met à jour son pseudo
+     * @param email
+     * @param pseudo
+     * @return l'utilisateur créé ou mis à jour
+     */
+    public User createOrUpdate(String email, String pseudo, String password) {
+      User u = new User();
+      u.setUsername(pseudo);
+      u.setMail(email);
+      u.setPwd(password);
+      em.merge(u);
+      return u;
+    }
+    
+    
+	
+}
---
+++ src/main/java/fr/univ_lyon1/etu/ewide/form/RegisterForm.java
@@ -0,0 +1,47 @@
+package fr.univ_lyon1.etu.ewide.form;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class RegisterForm {
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String username;
+
+    @NotNull
+    @Size(min=3, max=30)
+    private String email;
+    
+    @NotNull
+    @Size(min=2, max=30)
+    private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
+
+   
+}
