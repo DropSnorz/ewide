0699fccd176a33a818dcfb363f883ce1c0c4b345
Steven LEFEBVRE <Emulord@users.noreply.github.com>
2016-11-23 15:04:53 +0100

    * re-fix "Previous versions" button for Linux

---
+++ src/main/java/fr/univ_lyon1/etu/ewide/controller/UsersManagerController.java
@@ -0,0 +1,186 @@
+package fr.univ_lyon1.etu.ewide.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+
+
+
+
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.univ_lyon1.etu.ewide.dao.ProjectDAO;
+import fr.univ_lyon1.etu.ewide.dao.RoleDAO;
+import fr.univ_lyon1.etu.ewide.dao.UserDAO;
+import fr.univ_lyon1.etu.ewide.model.Project;
+import fr.univ_lyon1.etu.ewide.model.Role;
+import fr.univ_lyon1.etu.ewide.model.User;
+import fr.univ_lyon1.etu.ewide.service.AuthenticationUserService;
+
+@Transactional
+@Controller
+@RequestMapping(value="/project")
+public class UsersManagerController {
+	
+	@Autowired
+	AuthenticationUserService authenticationUserService;
+	
+	 @Autowired
+	 public  UserDAO usersDAO;
+	 
+	 @Autowired
+	 RoleDAO roleDAO;
+	 
+	 @Autowired
+	 ProjectDAO projectDAO;
+	 
+	 public static List<String> roles=Arrays.asList("MANAGER","DEVELOPER","REPORTER"); 
+	 
+	 
+	 /**
+	  * gives the list of the users with their role
+	  * @param Model 
+	  * @return view usersmanager
+	  * @throws IOException
+	  */
+	 @RequestMapping(value ="/{projectID}/users_manager", method = RequestMethod.GET)
+	 @PreAuthorize("@userRoleService.isMember(#projectID)")
+	 public ModelAndView usersManager(ModelMap Model, @PathVariable("projectID") int projectID) throws IOException{
+	     
+		 	List<User> listUsers = usersDAO.getAllUsersByProjectID(projectID);
+		 	User user = authenticationUserService.getCurrentUser();
+		 	Role role=roleDAO.getRoleByUserIdAndProjectId(user.getUserID(), projectID);
+	        ModelAndView model = new ModelAndView("usersmanager");
+	        
+	        //set of roles that exist (for the select)
+	        model.addObject("roles",roles);
+	        
+	        //role of the actual user (to display or not deleted button)
+	        model.addObject("userrole",role.getRole());
+	          
+	        //list of users with their role
+	        model.addObject("listUsers", listUsers);
+	        model.setViewName("usersmanager");
+			model.addObject("projectId",projectID);
+	        return model;
+	    }
+	 
+
+	 /**
+	  * to change the roles of the users
+	  *	if the role is null, user will be deleted from the project
+	  * @param allRequestParams (HashMap<String,String>)
+	  * @param projectID (int)
+	  * @return
+	  */
+	 @RequestMapping(value="/{projectID}/users_manager",method=RequestMethod.POST)
+	 @PreAuthorize("@userRoleService.isManager(#projectID)")
+	 public ModelAndView changeRoles(@RequestParam HashMap<String,String> allRequestParams,@PathVariable("projectID") int projectID){
+		 	allRequestParams.remove("_csrf");
+		 
+		 	ModelAndView model = new ModelAndView("usersmanager");
+		 
+		 	User user1 = authenticationUserService.getCurrentUser();
+		 	Role role=roleDAO.getRoleByUserIdAndProjectId(user1.getUserID(), projectID);
+	        
+	        //set of roles that exist (for the select)
+	        model.addObject("roles",roles);
+	        
+	        //role of the actual user (to display or not deleted button)
+	        model.addObject("userrole",role.getRole());
+	        
+	        
+		 	//must as at less a manager
+		 	if(allRequestParams.containsValue("MANAGER")){
+			  	for(Map.Entry<String, String>node : allRequestParams.entrySet()){
+			  		User user=usersDAO.getUserByUsername(node.getKey());
+			  		Project project=projectDAO.getProjectById(projectID);
+			  		//user can be deleted
+			  		if(node.getValue().equals("null")|| node.getValue()==null){
+			  			roleDAO.deleteRole(user,project);
+			  		}
+			  		//user can be updated
+			  		else{
+			  			roleDAO.updateRole(user, project, node.getValue());
+			  		}
+			  	}
+			  	model.addObject("success", "The users have been updated.");
+
+		 	}
+		 	//there isn't any manager 
+		 	else{
+		 		//get the roles again
+		 		model.addObject("error", "The project must have at less a manager.");
+	
+		 	}
+		 	List<User> listUsers = usersDAO.getAllUsersByProjectID(projectID);
+	        model.addObject("listUsers", listUsers);
+			model.addObject("projectId",projectID);
+	        model.setViewName("usersmanager");
+			return model;
+	 }
+	 
+	 /**
+	  * display users that can be adding to the project
+	  * @param username (String)
+	  * @return (ArrayList<User>) list of names
+	  */
+	 @RequestMapping(value="/users",method=RequestMethod.GET)
+	 @ResponseBody
+	 public ArrayList<User> autocompleteusers(@RequestParam String username){
+		 User user = authenticationUserService.getCurrentUser();
+		 ArrayList<User> listUsers = (ArrayList<User>) usersDAO.getUsersStartedwith(username,user);
+		 return  listUsers;
+		 
+	 }
+	 
+	 
+	 /**
+	  * add a user to the project if it doesn't exist
+	  * @param username (String)
+	  * @param projectID (int)
+	  * @param role (model)
+	  * @return
+	  */
+	 @RequestMapping(value="/{projectID}/adduser", method=RequestMethod.POST)
+	 @ResponseBody
+	 @PreAuthorize("@userRoleService.isManager(#projectID)")
+	 public User adduser(@RequestParam String username, @PathVariable("projectID") int projectID, Role role){
+		 User user = usersDAO.getUserByUsername(username);
+		 Project project=projectDAO.getProjectById(projectID);
+		 		 
+		 if(!(roleDAO.getProjectIDByUser(user)!=null &&roleDAO.getProjectIDByUser(user).contains(project))){
+			 role.setUser(user);
+			 roleDAO.createRole(user, project,"DEVELOPER");
+			 if(roleDAO.getProjectIDByUser(user).contains(project)){
+				 return user;
+			 }
+		 }
+		 return null;
+	 }
+	 
+	
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,114 @@
+<%@ page language="java" contentType="text/html; charset=windows-1256"
+    pageEncoding="windows-1256"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ page isELIgnored="false" %>
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1256">
+	<title>EWIDE</title>
+	<link rel="shortcut icon" href="resources/images/favicon.ico"> 
+	<!-- CSS dependencies -->
+	<link rel="stylesheet" type="text/css" href="resources/bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="resources/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="resources/css/font-style.css" />
+	<link rel="stylesheet" type="text/css" href="resources/css/md-icons.css" />
+	<link rel="stylesheet" href="resources/css/jquery.scrollbar.css">
+	<link rel="stylesheet" type="text/css" href="resources/css/main.css"/>
+</head>
+<body>
+	<!-- Fixed navbar -->
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index">EWIDE</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					
+				</ul>
+
+			</div><!--/.nav-collapse -->
+		</div>
+	</nav>
+
+
+	<div class="container">
+		<div class="row">
+			<!-- Logout message -->
+			<c:if test="${param.logout!= null}">
+				<div class="col-md-12">
+					<div class="alert alert-success" role="alert">
+						 <i class="material-icons">check</i>
+						 You have successfully logged out!
+					</div>
+				</div>
+			</c:if>
+			<div class="col-md-6 col-border-right">
+				<div class="ewide-form">
+					<!-- Login error message -->
+					<c:if test="${param.error != null}">
+						<div class="alert alert-danger" role="alert">
+							<i class="material-icons">error</i>
+							<span class="sr-only">Error:</span>
+							${sessionScope.SPRING_SECURITY_LAST_EXCEPTION.message}
+						</div>
+					</c:if>
+					<form name="formlogin" class="form-signin" action="login" method="post">
+						<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
+						<h2 class="form-signin-heading">Se connecter</h2>
+						<label for="inputEmail" class="sr-only">Email address</label>
+						<input name="email" type="email" id="email" class="form-control" placeholder="Email address" autofocus><br>
+						<label for="inputPassword" class="sr-only">Password</label>
+						<input name="password" type="password" id="password" class="form-control" placeholder="Password">
+						<br>
+						<button class="btn btn-lg btn-primary btn-block pix-btn-primary" name="submit" type="submit">Sign in</button>
+					</form>
+					<br><br>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="ewide-form">
+				<spring:hasBindErrors name="registerForm">
+				<div class="alert alert-danger" role="alert">
+					<i class="material-icons">error</i>
+					<span class="sr-only">Error: </span>
+					<c:forEach var="error" items="${errors.allErrors}">
+							<b><spring:message message="${error}" /></b>		
+					</c:forEach>
+				</div>
+				</spring:hasBindErrors>
+					<form:form name="registerForm" class="form-signup" action="register" method="post">
+						<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
+						<h2 class="form-signup-heading">S'inscrire</h2>
+						<label for="inputUsername" class="sr-only">Email address</label>
+						<input name="username" type="text" id="username" class="form-control" placeholder="Username" autofocus><br>
+						<label for="inputEmail" class="sr-only">Email address</label>
+						<input name="email" type="email" id="email" class="form-control" placeholder="Email address"><br>
+						<label for="inputPassword" class="sr-only">Password</label>
+						<input name="password" type="password" id="password" class="form-control" placeholder="Password"><br>
+						<input name="confirmpassword" type="password" id="confirmpassword" class="form-control" placeholder="Confirm password">
+						<br>
+						<button class="btn btn-lg btn-success btn-block pix-btn-primary" name="submit" type="submit">Sign Up</button>
+					</form:form>
+					<br><br>
+				</div>
+			</div>
+		</div>
+	</div> <!-- /container -->
+	
+	<!-- Javascripts -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script type="text/javascript" src="resources/js/jquery-1.11.2.js"></script>
+	<script type="text/javascript" src="resources/js/jquery-ui.js"></script>
+	<script type="text/javascript" src="resources/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
